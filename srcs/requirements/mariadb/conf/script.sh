8fad04c8fde0acd1f4e72fae23f053f465fc00bf
usfachab <usfachb@gmail.com>
2024-01-06 14:14:04 +0000

    Unable to connect

--- srcs/requirements/mariadb/conf/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "CREATE DATABASE IF NOT EXISTS db;" | mysql
-echo "CREATE USER 'user'@'%' IDENTIFIED BY 'password';" | mysql
-echo "GRANT ALL PRIVILEGES ON db.* TO 'user'@'%';" | mysql
-echo "FLUSH PRIVILEGES;" | mysql
-echo "use mysql;UPDATE user SET Password=PASSWORD('toor') WHERE user='root'; \
-        UPDATE user SET plugin = '' WHERE user = 'root';FLUSH PRIVILEGES;" | mysql
-
--- srcs/requirements/wordpress/config/wp-config.php
+++ srcs/requirements/wordpress/config/wp-config.php
@@ -1,46 +1,13 @@
 <?php
-
 define( 'DB_NAME', 'db' );
-
 define( 'DB_USER', 'user' );
-
 define( 'DB_PASSWORD', 'password' );
-
 define( 'DB_HOST', 'mariadb:3306' );
-
 define( 'DB_CHARSET', 'utf8' );
-
 define( 'DB_COLLATE', '' );
-
-// define( 'AUTH_KEY',         'put your unique phrase here' );
-// define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-// define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-// define( 'NONCE_KEY',        'put your unique phrase here' );
-// define( 'AUTH_SALT',        'put your unique phrase here' );
-// define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-// define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-// define( 'NONCE_SALT',       'put your unique phrase here' );
-
-/**#@-*/
-
-// // Additional settings
-// define('WP_SITEURL', 'http://10.12.174.242:8888');
-// define('WP_HOME', 'http://10.12.174.242:8888');
-// define('WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content');
-// define('WP_CONTENT_URL', 'http://10.12.174.242:8888/wp-content');
-
-// define('WP_INSTALLING', false);
-// Set the default admin user details
-// define('ADMIN_USER', 'admin');
-// define('ADMIN_PASSWORD', 'admin_password');
-// define('ADMIN_EMAIL', 'admin@example.com');
-
 $table_prefix = 'wp_';
-
 define( 'WP_DEBUG', false );
-
 if ( ! defined( 'ABSPATH' ) ) {
         define( 'ABSPATH', __DIR__ . '/' );
 }
-
 require_once ABSPATH . 'wp-settings.php';
